7094fcd25313d0fa769e6eaae773eb60c6e4c940
zietzm <michael.zietz@gmail.com>
2025-02-10 08:33:11 -0800

    refactor: clean out nextjs

--- src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="max-w-4xl mx-auto py-6 w-full">
-      <div className="flex justify-end px-6">
-        <div className="text-gray-600">© 2024 Michael Zietz</div>
-      </div>
-    </footer>
-  );
-}
